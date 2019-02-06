45c3383f8783ae9f9ee59d4da44b001d29bb59b9
dlakhan <lekhan1991@yahoo.co.in>
2019-02-06 20:32:09 +0530

    Changed solidity version to 0.5.0 as truffle doesn't support 0.5.2

---
+++ contracts/PublishBook.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5.0;
+
+contract PublishBook {
+    struct BookInfo{
+        string bookTittle;
+        string authorName;
+        string ipfsHash;
+        address publisherAddress;
+    }
+
+    mapping(bytes32 => BookInfo) private fileinfo;
+
+    function addBookDetails (string memory _tittle, string memory _author, string memory _ipfshash, bytes32 _hash) private{       
+        fileinfo[_hash].bookTittle = _tittle;
+        fileinfo[_hash].authorName = _author;
+        fileinfo[_hash].ipfsHash = _ipfshash;
+        fileinfo[_hash].publisherAddress = msg.sender;
+    }  
+
+    
+
+
+}
