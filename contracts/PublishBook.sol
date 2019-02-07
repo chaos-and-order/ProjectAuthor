31550f8872afb3a50d07f6a081cf1d1803da71fd
richuak <richuakuttikattu@gmail.com>
2019-02-07 13:09:32 +0530

    Pricing and Commission added

--- contracts/PublishBook.sol
+++ contracts/PublishBook.sol
@@ -8,19 +8,29 @@ contract PublishBook {
         string bookTitle;
         string authorName;
         string ipfsHash;
-        address publisherAddress;
+        address publisherAddress;  //Payable address ??
+        uint256 saleCommission; //in percent (0-100 integers)
     }
 
     mapping(uint256 => BookInfo) private fileinfo;
 
+
+    mapping (uint256 => uint256) private setPrice; //comes from frontend
+
+
+
     //BookInfo private fileinfo;
 
-    function addBookDetails (string memory _title, string memory _author, string memory _ipfshash, uint256 _isbn) private{       
+    function addBookDetails (string memory _title, string memory _author, string memory _ipfshash, uint256 _isbn, uint256 _saleCommission) private{       
         fileinfo[_isbn].bookTitle = _title;
         fileinfo[_isbn].authorName = _author;
         fileinfo[_isbn].ipfsHash = _ipfshash;
         fileinfo[_isbn].publisherAddress = msg.sender;
+        fileinfo[_isbn].saleCommission = _saleCommission;
     }  
+
+
+
 
     
 
