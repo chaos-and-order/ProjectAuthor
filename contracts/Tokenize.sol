4677d074887403e707c1a73cdfdc7ae4fa0a4681
richuak <richuakuttikattu@gmail.com>
2019-02-08 11:32:58 +0530

    attempt to get unique tokenID. Won't work.

--- contracts/Tokenize.sol
+++ contracts/Tokenize.sol
@@ -1,7 +1,18 @@
 pragma solidity ^0.5.0;
 import "./IERC721.sol";
+import "./PublishBook.sol";
 
 
-contract Tokenize is IERC721 {
+contract Tokenize is IERC721, PublishBook {
+
+    uint256 isbn;   //assuming we get this value from somewhere in the system
+    uint256 counter = 0;
+
+
+    //won't work. gotta find a more elegant solution
+    uint256 tokenId = uint256(string(isbn)+string(counter));
+
+    
+
 
 }
