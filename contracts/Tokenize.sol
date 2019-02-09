20f716ddead452cc6f45a57083e88e8e174b893e
dlakhan <lekhan1991@yahoo.co.in>
2019-02-09 14:37:46 +0530

    Added token ID generation function and added it to mintwithURI function.

--- contracts/Tokenize.sol
+++ contracts/Tokenize.sol
@@ -5,29 +5,32 @@ import "./PublishBook.sol";
 
 contract Tokenize is IERC721, PublishBook {
 
-    uint256 isbn;   //assuming we get this value from somewhere in the system
-    //uint256 counter = 0; //will be used later. Not used for tokenId  generation now.
-
-
-    //tokenId resolved, hopefully
+    uint256 isbn;   //Value to come in from the front end.
     uint256 tokenId;
-    tokenId = uint256(keccak256(abi.encodePacked(isbn, now))%10000000);
+    address tokenBuyer; //address of token purchaser via front-end.
+
+    // Mapping from token ID to owner
+    mapping (uint256 => address) private _tokenOwner;
+
+    // Mapping from token ID to approved address
+    mapping (uint256 => address) private _tokenApprovals;
+
+    // Mapping from owner to number of owned token
+    mapping (address => Counters.Counter) private _ownedTokensCount;
+    
+    //tokenId a value that will be less than 10,000,000 
+    function generateTokenID() private onlyMinter returns(uint256){
+        uint256 tokenid = uint256(keccak256(abi.encodePacked(isbn, now))) % 10000000;
+        return(tokenid);
+    }
 
     
-    
-    //assuming tokenId is resolved:
-
-    address to = msg.sender;  //the person who clicks the 'buy' button. Not the publisher
-
-    function _mint(address to, uint256 tokenId) internal {
-        
+    function _mint(address to) internal onlyMinter {
          require(to != address(0));
          require(!_exists(tokenId));
-
-        // _tokenOwner[tokenId] = to;
-        // _ownedTokensCount[to].increment();
-
-        // emit Transfer(address(0), to, tokenId);
+        _tokenOwner[tokenId] = to;
+        _ownedTokensCount[to].increment();
+        emit Transfer(address(0), to, tokenId);
     }
 
 
@@ -38,21 +41,24 @@ contract Tokenize is IERC721, PublishBook {
      * @param uri string URI to assign
      */
     function _setTokenURI(uint256 tokenId, string memory uri) internal {
-        // require(_exists(tokenId));
-        // _tokenURIs[tokenId] = uri;
+        require(_exists(tokenId));
+        _tokenURIs[tokenId] = uri;
     }
 
     /**
      * @dev Function to mint tokens
      * @param to The address that will receive the minted tokens.
-     * @param tokenId The token id to mint.
+     * @param tokenId The token id to mint. NOT IN USE HERE
      * @param tokenURI The token URI of the minted token.
      * @return A boolean that indicates if the operation was successful.
      */
-    function mintWithTokenURI(address to, uint256 tokenId, string memory tokenURI) public onlyMinter returns (bool) {
-        // _mint(to, tokenId);
-        // _setTokenURI(tokenId, tokenURI);
-        // return true;
+
+     //TO-DO: Check if buyer has enough ether before minting
+    function mintWithTokenURI(address to, string memory tokenURI) public onlyMinter returns (bool) {
+         tokenId = generateTokenID();        
+        _mint(to, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+        return true;
     }
 
 
