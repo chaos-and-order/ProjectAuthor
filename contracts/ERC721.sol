2ca8dbdfe024fb8762f5ff883fdc7c922ccba430
Richu A K <richuakuttikattu@gmail.com>
2019-02-11 16:20:19 +0530

    Basic functions added

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -4,8 +4,10 @@ import "./IERC721";
 
 contract ERC721 is IERC721{
 
-    /*
 
+    //TO DO: As user logs in, he needs to see his current libraby, without sacrificing privacy.
+    
+    /*
     gotta see how to incorporate this fruitfully
     constructor () public {
         // register the supported interfaces to conform to ERC721 via ERC165
@@ -14,7 +16,7 @@ contract ERC721 is IERC721{
     */
 
     /*
-
+    NOT IMPLEMENTING THIS METHOD, BECAUSE THERE IS NO CONCEPT OF BALANCE HERE, 
     ownedTokensCount uses Counter.counters  ;  gotta figure that out.
     function balanceOf(address owner) public view returns (uint256) {
         require(owner != address(0));
@@ -22,8 +24,61 @@ contract ERC721 is IERC721{
     }
     */
 
-    
+    function _exists(uint256 tokenId) internal view returns (bool) {
+        address owner = _tokenOwner[tokenId];
+        return owner != address(0);
+    }
 
 
+    function ownerOf(uint256 tokenId) public view returns (address) {
+        address owner = _tokenOwner[tokenId];
+        require(owner != address(0));
+        return owner;
+    }
+
+    function approve(address to, uint256 tokenId) public {
+        address owner = ownerOf(tokenId);
+        require(to != owner);
+        require(msg.sender == owner); // || isApprovedForAll(owner, msg.sender));  letting go of this function for now
+        //isApprovedForAll and approvalForAll lets someone send all the tokens in the account. Not relevant for us.
+        _tokenApprovals[tokenId] = to;
+        emit Approval(owner, to, tokenId);
+    }
+
+    function getApproved(uint256 tokenId) public view returns (address) {
+        require(_exists(tokenId));
+        return _tokenApprovals[tokenId];
+    }
+
+    function _isApprovedOrOwner(address spender, uint256 tokenId) internal view returns (bool) {
+        address owner = ownerOf(tokenId);
+        return (spender == owner || getApproved(tokenId) == spender);
+    }
+
+    function transferFrom(address from, address to, uint256 tokenId) public {
+        require(_isApprovedOrOwner(msg.sender, tokenId));
+
+        _transferFrom(from, to, tokenId);
+    }
+
+    function _transferFrom(address from, address to, uint256 tokenId) internal {
+        require(ownerOf(tokenId) == from);
+        require(to != address(0));
+
+        _clearApproval(tokenId);
+
+        //_ownedTokensCount[from].decrement();          Because we aren't using counters as of now
+        //_ownedTokensCount[to].increment();            But the chicken's gonna come to roost soon. BalanceOf's coming for you.
+
+        _tokenOwner[tokenId] = to;
+
+        emit Transfer(from, to, tokenId);
+    }
+
+    function _clearApproval(uint256 tokenId) private {
+        if (_tokenApprovals[tokenId] != address(0)) {
+            _tokenApprovals[tokenId] = address(0);
+        }
+    }
 
 }
