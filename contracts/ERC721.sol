b2747097c84bf0179fb190be550aa63da74771ae
Richu A K <richuakuttikattu@gmail.com>
2019-02-11 15:25:49 +0530

    ERC721 as a prelude to Resale begun

---
+++ contracts/ERC721.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.5.0;
+import "./IERC721";
+
+
+contract ERC721 is IERC721{
+
+    /*
+
+    gotta see how to incorporate this fruitfully
+    constructor () public {
+        // register the supported interfaces to conform to ERC721 via ERC165
+        _registerInterface(_INTERFACE_ID_ERC721);
+    }
+    */
+
+    /*
+
+    ownedTokensCount uses Counter.counters  ;  gotta figure that out.
+    function balanceOf(address owner) public view returns (uint256) {
+        require(owner != address(0));
+        return _ownedTokensCount[owner].current();
+    }
+    */
+
+    
+
+
+
+}
